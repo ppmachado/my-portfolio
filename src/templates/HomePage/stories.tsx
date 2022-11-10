eaf15e3d003687d4346fe04aba442e71d20c5559
paula <ppmachado.dev@gmail.com>
2022-11-10 11:40:35 -0300

    feat: homepage template created

---
+++ src/templates/HomePage/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import HomePage from '.'
+
+export default {
+  title: 'Components/HomePage',
+  component: HomePage
+} as Meta
+
+export const Default: Story = () => <HomePage />
