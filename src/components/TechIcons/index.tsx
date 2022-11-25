826668674fb5f1b2b42b0d0a0441044d75d69711
paula <ppmachado.dev@gmail.com>
2022-11-25 15:12:14 -0300

    feat: techicons component created

---
+++ src/components/TechIcons/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+
+const TechIcons = () => (
+  <S.Wrapper>
+    <S.Content>
+      <S.UseHtml5 />
+      <S.UseCss3 />
+      <S.UseJs />
+      <S.UseTypescript />
+      <S.UseReact />
+      <S.UseNext />
+      <S.UseStyled />
+      <S.UseGit />
+      <S.UseGithub />
+      <S.UseStorybook />
+      <S.UseGraphql />
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default TechIcons
