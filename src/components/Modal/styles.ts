282262f616e658b05757e6c587961fc84f350af1
paula <ppmachado.dev@gmail.com>
2022-12-12 13:54:53 -0300

    style: modal style done

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,7 +1,27 @@
 import styled, { css } from 'styled-components'
+import UseBox from '@mui/material/Box'
+import Dialog from '@mui/material/Dialog'
 
-export const Wrapper = styled.div`
+export const Box = styled(UseBox)`
   ${({ theme }) => css`
-    background: ${theme.colors.white};
+    border-radius: 8px;
+    border: none;
+    outline: ${theme.colors.primaryLight};
+
+    margin: 20px;
   `}
+`
+
+export const Span = styled.span`
+  ${({ theme }) => css`
+    font-family: ${theme.font.family02};
+    font-size: ${theme.font.sizes.small};
+    text-align: justify;
+
+    margin: 20px 20px;
+  `}
+`
+
+export const Modal = styled(Dialog)`
+  margin: 20px;
 `
