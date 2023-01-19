62ff679360157701442a9d523525aa8b422755dc
paula <ppmachado.dev@gmail.com>
2023-01-19 12:01:52 -0300

    fix: indentation fixed at app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyle from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ThemeProvider theme={theme}>
+  return (
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
-      <GlobalStyle  />
+      <GlobalStyle />
     </ThemeProvider>
+  )
 }
