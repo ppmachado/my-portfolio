d1d13834fbdd3d481cdc72141924888d184f817d
paula <ppmachado.dev@gmail.com>
2023-01-20 12:13:00 -0300

    feat: head added to app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyle from 'styles/global'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Head>
+        <title>Web Developer - Paula Machado</title>
+      </Head>
       <GlobalStyle />
     </ThemeProvider>
   )
